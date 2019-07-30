530dd18d355e71058a03e5a8542cb34413603a40
Peter Imrie <peter.imrie@jembi.org>
2019-07-30 16:44:30 +0200

    Added functionality to remove All Reports and Selected Reports for users who shouldn't have access

--- redcap_every_page_top.php
+++ redcap_every_page_top.php
@@ -1,6 +1,6 @@
 <?php
 
-if (strpos(PAGE, 'DataEntry') !== false) 
+if (strpos(PAGE, 'DataEntry') !== false || strpos(PAGE, 'DataExport') !== false) 
 {
  	$user_rights = REDCap::getUserRights(strtolower(USERID));
 	$user_rights = $user_rights[USERID];
@@ -8,24 +8,42 @@ if (strpos(PAGE, 'DataEntry') !== false)
 	$group_id = $user_rights['group_id'];
 	
 	// Right now we are hiding the dashboard if a user is not in a DAG and doesn't have create records rights 
-	if ($group_id == 0 && $user_rights['record_create'] == 0) {
+	if ($group_id == 0 && $user_rights['record_create'] == 0 && strpos(PAGE, 'DataEntry') !== false) {
    			// Hide Dashboard
 			$hide_status_dashboard = true;
-			}
-		if ($hide_status_dashboard) {
-			?>
-			   <style>
+	}
+	if ($group_id == 0 && $user_rights['record_create'] == 0 && strpos(PAGE, 'DataExport') !== false) {
+   			// Hide Dashboard
+			$hide_all_reports = true;
+	}
+	if ($hide_status_dashboard) {
+		?>
+		   <style>
 			      #center {opacity: 0;}
-			   </style>
-	   		<script>
-       				$(document).ready(function() {
-					$('#center').hide();	
-				        // Show the center again...
-				        $('#center').animate({opacity: 1}, 250);
-				});      
-			 </script>
-			<?php
+		   </style>
+	   	   <script>
+			$(document).ready(function() {
+				$('#center').hide();	
+			        // Show the center again...
+			        $('#center').animate({opacity: 1}, 250);
+			});      
+		   </script>
+		<?php
+	}
+	if ($hide_all_reports) {
+		?>
+		   <style>
+			#reprow_ALL {display:none;}
+			#reprow_SELECTED {display:none;}
+			#center {opacity: 0;}
+		   </style>
+	   	   <script>
+			$(document).ready(function() {
+				// Show the center again...
+			        $('#center').animate({opacity: 1}, 250);
+			});      
+		   </script>
+		<?php
 }
 }
-
 
